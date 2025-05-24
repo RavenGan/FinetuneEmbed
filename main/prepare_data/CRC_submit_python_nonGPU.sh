3ee08b77a1f0d7f156744dfd47bf66e52004fd0f
RavenGan <dailin.raven.gan@gmail.com>
2025-05-24 14:00:13 -0400

    prepare gene name embedding train test eval

--- main/prepare_data/CRC_submit_python_nonGPU.sh
+++ main/prepare_data/CRC_submit_python_nonGPU.sh
@@ -3,7 +3,7 @@
 #$ -m abe               # Send mail when job begins, ends and aborts
 #$ -q long              # Specify queue
 #$ -pe smp 1            # Specify number of cores to use.
-#$ -N Class15_split     # Specify job name
+#$ -N GeneName_split     # Specify job name
 
 module load conda
 source activate base
@@ -11,4 +11,5 @@ conda activate /afs/crc/user/d/dgan/.conda/envs/Diff.gene
 
 export PYTHONPATH="/afs/crc.nd.edu/group/StatDataMine/dm011/Dailin_Gan/FinetuneEmbed:$PYTHONPATH"
 # python GeneName_embed.py
-python prepare_15class_TrainEvalTest_text.py
+# python prepare_15class_TrainEvalTest_text.py
+python prepare_TrainEvalTest_Name.py
--- main/prepare_data/prepare_TrainEvalTest_Name.py
+++ main/prepare_data/prepare_TrainEvalTest_Name.py
@@ -1,23 +1,43 @@
 import os
+import sys
+# sys.path.append('/Users/david/Desktop/FinetuneEmbed')
+sys.path.append('/afs/crc.nd.edu/group/StatDataMine/dm011/Dailin_Gan/FinetuneEmbed')
 import json
 import pickle
 import mygene
+import pandas as pd
 # Specify the working directory
 # os.chdir('/Users/david/Desktop/FinetuneEmbed')
 os.chdir('/afs/crc.nd.edu/group/StatDataMine/dm011/Dailin_Gan/FinetuneEmbed')
 # load necessary functions
 from mod.utils import *
 
-# load gene ncbi descriptions
-with open("./data/gene_text/hs_ncbi_gene_text.json", "r") as file:
-    gene_descriptions = json.load(file)
-
-with open(f"./data/embeddings/GPT_3_5_gene_embeddings_fromGenePT.pickle", "rb") as fp:
-    GPT_3_5_gene_embeddings = pickle.load(fp)
+# load the gene name embeddings
+with open(f"./data/embeddings/GeneName_embed.pickle", "rb") as fp:
+    gene_name_embeddings = pickle.load(fp)
 
 random_states = list(range(41, 51)) # set up the random seeds
 eva_size = 0.1 # validation data proportions
 test_size = 0.1 # test data proportions
+
+### 15 class genes----------------------------------------------------------------
+gene_class = pd.read_csv('./data/Multi_class/gene_info_table_15classes.csv')
+gene_class_ls = list(gene_class['gene_name'])
+
+overlap_genes = list(set(gene_class_ls) & set(gene_name_embeddings.keys())) # find the intersected genes
+gene_class_sub = gene_class[gene_class['gene_name'].isin(overlap_genes)]
+
+genes = list(gene_class_sub['gene_name'])
+labels = list(gene_class_sub['gene_type'])
+save_dir = "./data/Multi_class/TrainEvalTestData_Name"
+# Create the directory if it does not exist
+os.makedirs(save_dir, exist_ok=True)
+
+# Split the data multiple times
+for random_state in random_states:
+    TrainEvalTest_Name_split(genes, labels, save_dir, 
+                        test_size=test_size, eval_size=eva_size, random_state=random_state)
+
 
 ### Long- vs short- range TFs----------------------------------------------------------------
 # The input data used here are downloaded from the geneformer paper Hugging Face website 
@@ -35,20 +55,19 @@ short_range_query = mg.querymany(short_range_tf_gene, species='human')
 long_range_gene_name = [x['symbol'] for x in long_range_query]
 short_range_gene_name = [x['symbol'] for x in short_range_query if 'symbol' in x]
 
-long_range_tf_gene = list(set(long_range_gene_name) & set(gene_descriptions.keys())) # find the intersected genes
-short_range_tf_gene = list(set(short_range_gene_name) & set(gene_descriptions.keys())) # find the intersected genes
-
-long_range_tf_gene = list(set(long_range_tf_gene) & set(GPT_3_5_gene_embeddings.keys())) # find the intersected genes
-short_range_tf_gene = list(set(short_range_tf_gene) & set(GPT_3_5_gene_embeddings.keys())) # find the intersected genes
+long_range_tf_gene = list(set(long_range_gene_name) & set(gene_name_embeddings.keys())) # find the intersected genes
+short_range_tf_gene = list(set(short_range_gene_name) & set(gene_name_embeddings.keys())) # find the intersected genes
 
 genes = long_range_tf_gene + short_range_tf_gene
 # 1 for long-range TF, 0 for short-range TF
 labels = [1] * len(long_range_tf_gene) + [0] * len(short_range_tf_gene) 
-save_dir = "./data/long_vs_shortTF/TrainEvalTestData"
+save_dir = "./data/long_vs_shortTF/TrainEvalTestData_Name"
+# Create the directory if it does not exist
+os.makedirs(save_dir, exist_ok=True)
 
 # Split the data multiple times
 for random_state in random_states:
-    TrainEvalTest_split(genes, labels, gene_descriptions, save_dir, 
+    TrainEvalTest_Name_split(genes, labels, save_dir, 
                         test_size=test_size, eval_size=eva_size, random_state=random_state)
     
 
@@ -66,21 +85,19 @@ in_sensitive_query = mg.querymany(insensitive, species='human')
 sensitive_gene_name = [x['symbol'] for x in sensitive_query]
 insensitive_gene_name = [x['symbol'] for x in in_sensitive_query if 'symbol' in x]
 
-sensitive_gene = list(set(sensitive_gene_name) & set(gene_descriptions.keys())) # find the intersected genes
-insensitive_gene = list(set(insensitive_gene_name) & set(gene_descriptions.keys())) # find the intersected genes
-
-sensitive_gene = list(set(sensitive_gene) & set(GPT_3_5_gene_embeddings.keys())) # find the intersected genes
-insensitive_gene = list(set(insensitive_gene) & set(GPT_3_5_gene_embeddings.keys())) # find the intersected genes
+sensitive_gene = list(set(sensitive_gene_name) & set(gene_name_embeddings.keys())) # find the intersected genes
+insensitive_gene = list(set(insensitive_gene_name) & set(gene_name_embeddings.keys())) # find the intersected genes
 
 genes = sensitive_gene + insensitive_gene
  # 1 for sensitive, 0 for insensitive
 labels = [1] * len(sensitive_gene) + [0] * len(insensitive_gene)
-save_dir = "./data/DosageSensitivity/TrainEvalTestData"
+save_dir = "./data/DosageSensitivity/TrainEvalTestData_Name"
 
 # Split the data multiple times
 for random_state in random_states:
-    TrainEvalTest_split(genes, labels, gene_descriptions, save_dir, 
+    TrainEvalTest_Name_split(genes, labels, save_dir, 
                         test_size=test_size, eval_size=eva_size, random_state=random_state)
+    
 
 ###  Methylation state prediction---------------------------------------------------------------
 # The csv files are downloaded from https://huggingface.co/datasets/ctheodoris/Genecorpus-30M/
@@ -98,20 +115,17 @@ lysine_query = mg.querymany(lysine_gene_labels, species='human')
 bivalent_gene_name = [x.get('symbol', '') for x in bivalent_query]
 lysine_gene_name = [x.get('symbol', '') for x in lysine_query if 'symbol' in x]
 
-bivalent_gene = list(set(bivalent_gene_name) & set(gene_descriptions.keys())) # find the intersected genes
-lysine_gene = list(set(lysine_gene_name) & set(gene_descriptions.keys())) # find the intersected genes
-
-bivalent_gene = list(set(bivalent_gene) & set(GPT_3_5_gene_embeddings.keys())) # find the intersected genes
-lysine_gene = list(set(lysine_gene) & set(GPT_3_5_gene_embeddings.keys())) # find the intersected genes
+bivalent_gene = list(set(bivalent_gene_name) & set(gene_name_embeddings.keys())) # find the intersected genes
+lysine_gene = list(set(lysine_gene_name) & set(gene_name_embeddings.keys())) # find the intersected genes
 
 genes = bivalent_gene + lysine_gene
 # 1 for bivalent_gene, 0 for lysine_gene
 labels = [1] * len(bivalent_gene) + [0] * len(lysine_gene)
-save_dir = "./data/MethylationState/bivalent_vs_lys4/TrainEvalTestData"
+save_dir = "./data/MethylationState/bivalent_vs_lys4/TrainEvalTestData_Name"
 
 # Split the data multiple times
 for random_state in random_states:
-    TrainEvalTest_split(genes, labels, gene_descriptions, save_dir, 
+    TrainEvalTest_Name_split(genes, labels, save_dir, 
                         test_size=test_size, eval_size=eva_size, random_state=random_state)
 
 ## Bivalent vs. no methyl
@@ -128,18 +142,15 @@ no_methylation_query = mg.querymany(no_methylation_gene_labels, species='human')
 bivalent_gene_name = [x.get('symbol', '') for x in bivalent_query]
 no_methylation_gene_name = [x.get('symbol', '') for x in no_methylation_query if 'symbol' in x]
 
-bivalent_gene = list(set(bivalent_gene_name) & set(gene_descriptions.keys())) # find the intersected genes
-no_methylation_gene = list(set(no_methylation_gene_name) & set(gene_descriptions.keys())) # find the intersected genes
-
-bivalent_gene = list(set(bivalent_gene) & set(GPT_3_5_gene_embeddings.keys())) # find the intersected genes
-no_methylation_gene = list(set(no_methylation_gene) & set(GPT_3_5_gene_embeddings.keys())) # find the intersected genes
+bivalent_gene = list(set(bivalent_gene_name) & set(gene_name_embeddings.keys())) # find the intersected genes
+no_methylation_gene = list(set(no_methylation_gene_name) & set(gene_name_embeddings.keys())) # find the intersected genes
 
 genes = bivalent_gene + no_methylation_gene
  # 1 for bivalent_gene, 0 for no_methylation_gene
 labels = [1] * len(bivalent_gene) + [0] * len(no_methylation_gene)
-save_dir = "./data/MethylationState/bivalent_vs_no_methyl/TrainEvalTestData"
+save_dir = "./data/MethylationState/bivalent_vs_no_methyl/TrainEvalTestData_Name"
 
 # Split the data multiple times
 for random_state in random_states:
-    TrainEvalTest_split(genes, labels, gene_descriptions, save_dir, 
+    TrainEvalTest_Name_split(genes, labels, save_dir, 
                         test_size=test_size, eval_size=eva_size, random_state=random_state)
