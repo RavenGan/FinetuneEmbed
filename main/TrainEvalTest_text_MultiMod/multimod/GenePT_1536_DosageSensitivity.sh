309e32176929fbf652b9e8f45d22346d9de4101f
RavenGan <dailin.raven.gan@gmail.com>
2025-05-27 14:27:28 -0400

    GenePT task classification scripts

---
+++ main/TrainEvalTest_text_MultiMod/multimod/GenePT_1536_DosageSensitivity.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#$ -M dgan@nd.edu
+#$ -m abe               # Send mail when job begins, ends and aborts
+#$ -q long              # Specify queue
+#$ -pe smp 1            # Specify number of cores to use.
+#$ -N GenePT_DosageSensitivity     # Specify job name
+
+module load conda
+source activate base
+conda activate /afs/crc/user/d/dgan/.conda/envs/Diff.gene
+
+export PYTHONPATH="/afs/crc.nd.edu/group/StatDataMine/dm011/Dailin_Gan/FinetuneEmbed:$PYTHONPATH"
+python Gene_level_task_GenePT_1536.py \
+    --task "DosageSensitivity" \
+    --embedding_data "TrainEvalTestData" \
+    --embedding_type "text_embedding" \
+    --do_cv \
+    --do_pca \
+    --n_PCs 20 \
+    --save_root "./res/2025_0527"
+
+python Gene_level_task_GenePT_1536.py \
+    --task "DosageSensitivity" \
+    --embedding_data "TrainEvalTestData" \
+    --embedding_type "text_embedding" \
+    --do_cv \
+    --n_PCs 20 \
+    --save_root "./res/2025_0527"
+
+python Gene_level_task_GenePT_1536.py \
+    --task "DosageSensitivity" \
+    --embedding_data "TrainEvalTestData" \
+    --embedding_type "text_embedding" \
+    --do_pca \
+    --n_PCs 20 \
+    --save_root "./res/2025_0527"
+
+python Gene_level_task_GenePT_1536.py \
+    --task "DosageSensitivity" \
+    --embedding_data "TrainEvalTestData" \
+    --embedding_type "text_embedding" \
+    --n_PCs 20 \
+    --save_root "./res/2025_0527"
+
+
+python Gene_level_task_GenePT_1536.py \
+    --task "DosageSensitivity" \
+    --embedding_data "TrainEvalTestData_Name" \
+    --embedding_type "name_embedding" \
+    --do_cv \
+    --do_pca \
+    --n_PCs 20 \
+    --save_root "./res/2025_0527"
+
+
+python Gene_level_task_GenePT_1536.py \
+    --task "DosageSensitivity" \
+    --embedding_data "TrainEvalTestData_Name" \
+    --embedding_type "name_embedding" \
+    --do_cv \
+    --n_PCs 20 \
+    --save_root "./res/2025_0527"
+
+python Gene_level_task_GenePT_1536.py \
+    --task "DosageSensitivity" \
+    --embedding_data "TrainEvalTestData_Name" \
+    --embedding_type "name_embedding" \
+    --do_pca \
+    --n_PCs 20 \
+    --save_root "./res/2025_0527"
+
+python Gene_level_task_GenePT_1536.py \
+    --task "DosageSensitivity" \
+    --embedding_data "TrainEvalTestData_Name" \
+    --embedding_type "name_embedding" \
+    --n_PCs 20 \
+    --save_root "./res/2025_0527"
--- main/TrainEvalTest_text_MultiMod/multimod/gene_level_task_GenePT_1536.py
+++ main/TrainEvalTest_text_MultiMod/multimod/gene_level_task_GenePT_1536.py
@@ -1,47 +1,60 @@
 import os
+import sys
 import pickle
+import argparse
+# sys.path.append('/Users/david/Desktop/FinetuneEmbed')
+sys.path.append('/afs/crc.nd.edu/group/StatDataMine/dm011/Dailin_Gan/FinetuneEmbed')
 
 # Specify the working directory
 # os.chdir('/Users/david/Desktop/FinetuneEmbed')
 os.chdir('/afs/crc.nd.edu/group/StatDataMine/dm011/Dailin_Gan/FinetuneEmbed')
 from mod.utils import *
 
-# with open(f"./data/embeddings/GPT_3_5_gene_embeddings_fromGenePT.pickle", "rb") as fp:
-#     GPT_3_5_gene_embeddings = pickle.load(fp)
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Embedding evaluation pipeline")
 
-### Use text-embedding-3-large generated embeddings
-# with open(f"/afs/crc.nd.edu/user/d/dgan/TransferGPT/data_embedding/GPT_3_large_human_embeddings_filtered.pickle", "rb") as fp:
-#     GPT_3_5_gene_embeddings = pickle.load(fp)
+parser.add_argument("--task", type=str, default="long_vs_shortTF", help="Classification task name")
+parser.add_argument("--embedding_data", type=str, default="TrainEvalTestData", help="Directory for embedding data")
+parser.add_argument("--embedding_type", type=str, default="text_embedding", help="Embedding type")
+parser.add_argument("--do_cv", action="store_true", help="Whether to run cross-validation")
+parser.add_argument("--do_pca", action="store_true", help="Whether to apply PCA")
+parser.add_argument("--n_PCs", type=int, default=20, help="Number of principal components")
+parser.add_argument("--save_root", type=str, default="./res/2025_0526", help="Directory to save results")
 
-with open(f"./data/embeddings/GenePT_gene_embedding_ada_text_fromGenePT.pickle", "rb") as fp:
-     GPT_3_5_gene_embeddings = pickle.load(fp)
+args = parser.parse_args()
 
 random_states = list(range(41, 51)) # set up the random seeds
-do_pca = False
-do_cv = True
 
-## Long- vs short- range TFs
-# The input data used here are downloaded from Chen et al. (2020) 
-# (link: https://www-nature-com.stanford.idm.oclc.org/articles/s41467-020-16106-x).
-data_dir = "./data/long_vs_shortTF"
-save_csv_dir = "./res/2025_0206/GenePT_1536_long_vs_shortTF_auc.csv"
-multiple_run_TrainTest(data_dir, save_csv_dir, random_states, GPT_3_5_gene_embeddings, 
-                       do_pca, do_cv)
+# load the gene embedding dictionary
+with open(f"./data/embeddings/GenePT_gene_embedding_ada_text_fromGenePT.pickle", "rb") as fp:
+     embedding_dict = pickle.load(fp)
 
-## Dosage sensitive vs insensitive TFs
-data_dir = "./data/DosageSensitivity"
-save_csv_dir = "./res/2025_0206/GenePT_1536_DosageSensitivity_auc.csv"
-multiple_run_TrainTest(data_dir, save_csv_dir, random_states, GPT_3_5_gene_embeddings, 
-                       do_pca, do_cv)
+task = args.task
+embedding_data = args.embedding_data
+embedding_type = args.embedding_type
+do_cv = args.do_cv
+do_pca = args.do_pca
+n_PCs = args.n_PCs
+save_root = args.save_root
 
-## Bivalent vs. lys4
-data_dir = "./data/MethylationState/bivalent_vs_lys4"
-save_csv_dir = "./res/2025_0206/GenePT_1536_bivalent_vs_lys4_auc.csv"
-multiple_run_TrainTest(data_dir, save_csv_dir, random_states, GPT_3_5_gene_embeddings, 
-                       do_pca, do_cv)
+data_dir = f"./data/{task}/{embedding_data}"
 
-## Bivalent vs. no methyl
-data_dir = "./data/MethylationState/bivalent_vs_no_methyl"
-save_csv_dir = "./res/2025_0206/GenePT_1536_bivalent_vs_no_methyl_auc.csv"
-multiple_run_TrainTest(data_dir, save_csv_dir, random_states, GPT_3_5_gene_embeddings, 
-                       do_pca, do_cv)
+if do_cv==True and do_pca==True:
+     folder_name = f"PCA_CV_{embedding_type}"
+elif do_cv==True and do_pca==False:
+     folder_name = f"PCA_NoCV_{embedding_type}"
+elif do_cv==False and do_pca==True:
+     folder_name = f"NoPCA_CV_{embedding_type}"
+elif do_cv==False and do_pca==False:
+     folder_name = f"NoPCA_NoCV_{embedding_type}"
+
+
+model_name = "GenePT_1536"
+save_mod_name = "GenePT_1536"
+
+save_csv_dir = f"{save_root}/{folder_name}/" + save_mod_name + f"_{task}_NumRes.csv"
+os.makedirs(os.path.dirname(save_csv_dir), exist_ok=True)
+ROC_save_dir = f"{save_root}/{folder_name}/{task}/" + save_mod_name + "/"
+
+multiple_run_TrainTest(data_dir, save_csv_dir, random_states, 
+                       embedding_dict, ROC_save_dir, do_cv, do_pca, n_PCs)
