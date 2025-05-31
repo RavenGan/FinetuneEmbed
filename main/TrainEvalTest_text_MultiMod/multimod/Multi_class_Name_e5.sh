355ef0ffe4edd9548d93e8266da52ce143581d88
RavenGan <dailin.raven.gan@gmail.com>
2025-05-31 14:17:03 -0400

    correct name embedding save path

--- main/TrainEvalTest_text_MultiMod/multimod/Multi_class_Name_e5.sh
+++ main/TrainEvalTest_text_MultiMod/multimod/Multi_class_Name_e5.sh
@@ -14,10 +14,10 @@ export PYTHONPATH="/afs/crc.nd.edu/group/StatDataMine/dm011/Dailin_Gan/FinetuneE
 python MultiMod_Multi_class.py \
     --task "Multi_class" \
     --embedding_data "TrainEvalTestData_Name" \
-    --embedding_type "text_embedding" \
+    --embedding_type "name_embedding" \
     --do_cv \
     --n_PCs 20 \
-    --save_root "./res/2025_0530" \
+    --save_root "./res/2025_0531" \
     --model_name 'intfloat/e5-small-v2' \
     --save_mod_name "e5-small-v2"
 
@@ -25,8 +25,8 @@ python MultiMod_Multi_class.py \
 python MultiMod_Multi_class.py \
     --task "Multi_class" \
     --embedding_data "TrainEvalTestData_Name" \
-    --embedding_type "text_embedding" \
+    --embedding_type "name_embedding" \
     --n_PCs 20 \
-    --save_root "./res/2025_0530" \
+    --save_root "./res/2025_0531" \
     --model_name 'intfloat/e5-small-v2' \
     --save_mod_name "e5-small-v2"
